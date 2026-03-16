62665e782596be00b84b4f95f4247785d37c1279
EggsArntMemes <135548594+EggsArntMeme@users.noreply.github.com>
2026-03-16 13:18:12 -0600

    Added what I've worked on

---
+++ TheDuckTaphouse&Grill/functions.php
@@ -0,0 +1,26 @@
+<?php
+add_theme_support( 'block-template-parts' );
+
+function mytheme_enqueue_styles() {
+    // Enqueue the main theme stylesheet (style.css in the theme root)
+    wp_enqueue_style( 'mytheme-main-style', get_stylesheet_uri() );
+
+    // Enqueue an additional custom stylesheet (e.g., in a 'css' subfolder)
+    wp_enqueue_style( 'mytheme-custom-style', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_styles' );
+
+
+function disable_new_pages_for_users() {
+    if ( !current_user_can( 'manage_options' ) ) {
+        remove_post_type_support( 'page', 'create_posts' );
+        // Optional: Hide the "Add New" menu item
+        global $submenu;
+        if (isset($submenu['edit.php?post_type=page'])) {
+            unset($submenu['edit.php?post_type=page'][10]);
+        }
+    }
+}
+add_action( 'admin_menu', 'disable_new_pages_for_users' );
+
+?>
